86b70dc7ba63be2013ed1afa9d06093647391ca9
yxs66 <1284675135@qq.com>
2020-06-01 13:55:31 +0800

    Initial commit

---
+++ src/main/java/com/yyy/App.java
@@ -0,0 +1,13 @@
+package com.yyy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
